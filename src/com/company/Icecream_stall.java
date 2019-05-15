2229fd766ae8fd4ed50eff4f2192ee1a3a7ab6db
Jayashree <jayashree.kammu@gmail.com>
2019-05-15 08:17:34 +0530

    added functionality for menu items

--- src/com/company/Icecream_stall.java
+++ src/com/company/Icecream_stall.java
@@ -1,5 +1,37 @@
 package com.company;
+import java.util.Scanner;
+import java.lang.String;
+import java.util.ArrayList;
+
+class menu_items {
+}
 
 public class Icecream_stall {
+    public static void main(String[] args) {
+        int i;
+        ArrayList<menu_items> list = new ArrayList<menu_items>();
+        //Add a new ShoppingItem to the list
+        System.out.println();
+        System.out.println("enter the number of your item");
+        Scanner keyboard = new Scanner(System.in);
+        int total_item= keyboard.nextInt();
+        System.out.println("enter the items name");
+        String ice_cream_name = keyboard.nextLine();
+        String array[] = new String[total_item];
+        for (i = 0; i < total_item; i++) {
+            array[i] = keyboard.nextLine();
 
-}
+        }
+        System.out.println("Icecream menu list");
+        for (i = 0; i < total_item; i++) {
+            System.out.println("items :"+array[i]);
+        }
+
+
+
+        }
+
+
+    }
+
+
