b0bc85f434e23b5c4c2073dba567fae16ee11e74
Jayashree <jayashree.kammu@gmail.com>
2019-05-15 07:08:41 +0530

    added functionality to the cost calculation

---
+++ src/com/company/Icecream_stall.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class Icecream_stall {
+
+}
--- src/com/company/print_the_item_name.java
+++ src/com/company/print_the_item_name.java
@@ -6,11 +6,11 @@ import java.util.*;
 
 
 class list_the_item {
-    String name;
-    int quantity;
-    double price;
+    public String name;
+    public int quantity;
+    public double price;
 
-    list_the_item(String name, int quantity, double price) {
+    void show_detail(String name, int quantity, double price) {
         this.name = name;
         this.quantity = quantity;
         this.price = price;
@@ -18,10 +18,11 @@ class list_the_item {
     }
 
     public String getName() {
+
         return name;
     }
 
-    void calculate_the_cost_without_sales_tax() {
+    void calculate_the_cost() {
         double cost_of_the_item;
         System.out.println("price of the item without sales tax");
         cost_of_the_item = quantity * price;
@@ -29,38 +30,38 @@ class list_the_item {
 
     }
 
-    void show_total_quantity() {
-        double cost_of_each_item;
-        System.out.println("Normal price of the item");
-        cost_of_each_item = quantity * price;
-        System.out.println(name + " is " + cost_of_each_item);
 
 
-    }
-
-    void sales_tax_of_the_item() {
-        double total_quantity, sales_tax;
-        System.out.println("sales tax of the item");
-        sales_tax = (15.00 / 100) + price;
-        total_quantity = quantity * sales_tax;
-        System.out.println(name + " is " + total_quantity);
-    }
+void sales_tax_of_the_item() {
+    double total_quantity, sales_tax;
+    System.out.println("sales tax of the item");
+    sales_tax = (15.00 / 100) + price;
+    total_quantity = quantity * sales_tax;
+    System.out.println(name + " is " + total_quantity);
+}
 
 }
 
 class extract_item_from_list {
     void extract_the_item_from_the_sentence(String sentence) {
-
+        int length_of_array;
         String[] words = sentence.split(" ");
 //       for(String word :words)
 //           System.out.println(word);
         ArrayList<String> wordArrayList = new ArrayList<>(Arrays.asList(words));
-        String quantity = wordArrayList.get(0);
-        String Name = wordArrayList.get(1);
-        String cost = wordArrayList.get(3);
-        System.out.println(quantity + " is quantity");
-        System.out.println(Name + " is name");
-        System.out.println(cost + " is cost of pen");
+        length_of_array = wordArrayList.size();
+        if (length_of_array > 1) {
+            String quantity = wordArrayList.get(0);
+            int quantities = Integer.parseInt(quantity);
+            String Name = wordArrayList.get(1);
+            String cost = wordArrayList.get(3);
+            double costs = Double.parseDouble(cost);
+            System.out.println(quantities + " is quantity");
+            System.out.println(Name + " is name");
+            System.out.println(costs + " is cost of pen");
+            double sales_taxes = (15.00 / 100) + costs;
+            double cost_of_the_items = quantities * sales_taxes;
+            System.out.println(Name + " is " + cost_of_the_items);
 //       int j,length_of_array;
 //       length_of_array=wordArrayList.size();
 //       for(j=0;j<length_of_array;j++)
@@ -73,18 +74,31 @@ class extract_item_from_list {
 //       }
 
 
+        }
     }
 }
 
 public class print_the_item_name {
     public static void main(String[] args) {
         int i;
-        list_the_item[] item_array = new list_the_item[5];
-        list_the_item item_list1 = new list_the_item("pen", 2, 3);
-        list_the_item item_list2 = new list_the_item("pencil", 1, 1);
-        list_the_item item_list3 = new list_the_item("ball point pen", 2, 50);
-        list_the_item item_list4 = new list_the_item("marker", 3, 10);
-        list_the_item item_list5 = new list_the_item("scale", 2, 20);
+//        list_the_item[] item_array = new list_the_item[5];
+//        list_the_item item_list1 = new list_the_item("pen", 2, 3);
+//        list_the_item item_list2 = new list_the_item("pencil", 1, 1);
+//        list_the_item item_list3 = new list_the_item("ball point pen", 2, 50);
+//        list_the_item item_list4 = new list_the_item("marker", 3, 10);
+//        list_the_item item_list5 = new list_the_item("scale", 2, 20);
+         list_the_item[] item_array = new list_the_item[5];
+        list_the_item item_list1 = new list_the_item();
+        item_list1.show_detail("pen",2,3);
+        list_the_item item_list2 = new list_the_item();
+        item_list2.show_detail("pencil",1,1);
+        list_the_item item_list3 = new list_the_item();
+        item_list3.show_detail("ball point pen",2,50);
+        list_the_item item_list4 = new list_the_item();
+        item_list4.show_detail("marker",3,10);
+        list_the_item item_list5 = new list_the_item();
+        item_list5.show_detail("scale",2,20);
+
         item_array[0] = item_list1;
         item_array[1] = item_list2;
         item_array[2] = item_list3;
@@ -94,13 +108,13 @@ public class print_the_item_name {
         for (i = 0; i < (item_array.length) - 1; i++) {
             System.out.println(item_array[i].getName());
             System.out.println();
-            item_array[i].show_total_quantity();
+            item_array[i].calculate_the_cost();
             System.out.println();
             item_array[i].sales_tax_of_the_item();
             System.out.println();
         }
-        item_array[3].calculate_the_cost_without_sales_tax();
-        item_array[4].calculate_the_cost_without_sales_tax();
+        item_array[3].calculate_the_cost();
+        item_array[4].calculate_the_cost();
         Scanner items = new Scanner(System.in);
         System.out.println();
         System.out.print("enter the item name :");
