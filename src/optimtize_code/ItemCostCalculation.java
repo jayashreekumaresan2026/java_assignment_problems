8fa89ebbfb15397ecb01468244b2b71267ef9794
Jayashree <jayashree.kammu@gmail.com>
2019-05-15 13:43:15 +0530

    sales tax and cost calculated

--- src/optimtize_code/ItemCostCalculation.java
+++ src/optimtize_code/ItemCostCalculation.java
@@ -21,12 +21,29 @@ class ItemMenu {
         System.out.println(name + "   " + quantity + "   " + price + " ");
     }
 
+    double salesTaxCalculation() {
+        double total_quantity, sales_tax;
+        sales_tax = (15.00 / 100) + price;
+        total_quantity = quantity * sales_tax;
+        return total_quantity;
+    }
+}
 
+class CostCalculation {
+    double costCalculation(int quantityInInteger, double costInInteger) {
+        double itemCost;
+        System.out.println("Price of the item without sales tax");
+        itemCost = quantityInInteger * costInInteger;
+        return itemCost;
+
+    }
 }
 
 
 class ExtractCostQuantityName {
-    void getSentence(String sentence) {
+    double getSentence(String sentence) {
+        double itemCost;
+        CostCalculation object1 = new CostCalculation();
         String[] words = sentence.split(" ");
         ArrayList<String> wordArrayList = new ArrayList<>(Arrays.asList(words));
         String quantity = wordArrayList.get(0);
@@ -37,7 +54,8 @@ class ExtractCostQuantityName {
         System.out.println(quantityInInteger + " is quantity");
         System.out.println(itemName + " is name");
         System.out.println(costInInteger + " is cost of pen");
-
+        itemCost = object1.costCalculation(quantityInInteger, costInInteger);
+        return itemCost;
     }
 
 }
@@ -46,6 +64,7 @@ class ExtractCostQuantityName {
 public class ItemCostCalculation {
     public static void main(String[] args) {
         int i;
+        double totalSalesTax = 0;
         ItemMenu[] itemArray = new ItemMenu[3];
         ItemMenu itemList1 = new ItemMenu();
         itemList1.getItemDetail("   Pen", 2, 3);
@@ -58,11 +77,13 @@ public class ItemCostCalculation {
         itemArray[2] = itemList3;
         for (i = 0; i < (itemArray.length); i++) {
             itemArray[i].showItemName();
+            totalSalesTax += itemArray[i].salesTaxCalculation();
         }
+        System.out.println("total " + totalSalesTax);
         System.out.print("enter the item name :");
         Scanner keyboard = new Scanner(System.in);
         String sentence = keyboard.nextLine();
-        ExtractCostQuantityName CQN = new ExtractCostQuantityName();
-        CQN.getSentence(sentence);
+        ExtractCostQuantityName CostQuanName = new ExtractCostQuantityName();
+        CostQuanName.getSentence(sentence);
     }
 }
